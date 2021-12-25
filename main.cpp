592c1e42a44f41cc5c403ab5b8ce8e80fc0ed618
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2021-12-25 12:44:57 +0000

    backup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <cmath>
 #include <limits>
 #include <iostream>
+#include <string>
+#include <functional>
+#include <sstream>
+#include <iomanip>
+#include <numbers>
 
 template<typename T>
 struct Fraction
@@ -8,57 +13,100 @@ struct Fraction
     T numerator;
     T denominator;
 
-    bool isnan()   const { return std::isnan(numerator) || std::isnan(denominator); }
-    T fractional() const { return numerator / denominator; }
+    bool isnan()      const { return std::isnan(numerator) || std::isnan(denominator); }
+    bool isintegral() const { return denominator == 1.0; }
+    T    fractional() const { return numerator / denominator; }
 
     Fraction(T numerator, T denominator)
         : numerator(numerator), denominator(denominator)
     { }
 
+    // std::string str()
+    // {
+    //     std::stringstream ss;
+    //     ss << std::setprecision(30) << numerator << " / " << std::setprecision(30) << denominator;
+    //     return ss.str();
+    // }
+
     static Fraction<T> quiet_NaN()
     {
-        return Fraction<T>(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN());
+        return Fraction<T>(std::numeric_limits<T>::quiet_NaN(), std::numeric_limits<T>::quiet_NaN());
+    }
+
+    static Fraction<T> find(T value, T max_error, int iters)
+    {
+        Fraction<T> lower(0, 1);
+        Fraction<T> upper(1, 0);
+
+        T integral;
+        T fractional = std::modf(value, &integral);
+
+        if (fractional == 0.0)
+        {
+            return Fraction<T>(integral, 1);
+        }
+
+        while (iters--)
+        {
+            Fraction<T> middle(lower.numerator + upper.numerator, lower.denominator + upper.denominator);
+            T test = middle.fractional();
+
+            if (std::abs(test - fractional) <= max_error)
+            {
+                return Fraction<T>(middle.numerator + integral * middle.denominator, middle.denominator);
+            }
+
+            if (fractional > test)
+            {
+                lower = middle;
+            }
+            else
+            {
+                upper = middle;
+            }
+        }
+
+        return Fraction<T>::quiet_NaN();
     }
 };
 
 template<typename T>
-Fraction<T> find_fraction(T fractional, T max_error, int iters)
+struct UsefulFraction
 {
-    Fraction<T> lower(0, 1);
-    Fraction<T> upper(1, 0);
+    std::string format;
+    std::function<T(T)> operate;
+};
 
-    while (iters--)
-    {
-        Fraction<T> middle(lower.numerator + upper.numerator, lower.denominator + upper.denominator);
-        T test = middle.fractional();
-
-        if (std::abs(test - fractional) <= max_error)
-        {
-            return middle;
-        }
-
-        if (fractional > test)
-        {
-            lower = middle;
-        }
-        else
-        {
-            upper = middle;
-        }
-    }
-
-    return Fraction<T>::quiet_NaN();
-}
 
 int main(int, char**)
 {
-    auto f = find_fraction<double>(15.0f / 4.0f, 1E-12, 1000000);
-    if (!f.isnan())
+    std::array<UsefulFraction<double>, 7> uses
     {
-        std::cout << "Hello: " << f.numerator << '/' << f.denominator << '\n';
-    }
-    else
+        (UsefulFraction<double> {  "(%g) / (%g)",  [](double x) { return x; }}),
+
+        (UsefulFraction<double> { "π(%g) / (%g)",  [](double x) { return x / M_PI; }}),
+        (UsefulFraction<double> {  "(%g) / (π%g)", [](double x) { return x * M_PI; }}),
+
+        (UsefulFraction<double> { "e(%g) / (%g)",  [](double x) { return x / M_E; }}),
+        (UsefulFraction<double> {  "(%g) / (e%g)", [](double x) { return x * M_E; }}),
+
+        (UsefulFraction<double> { "√2⋅(%g) / (%g)",  [](double x) { return x / M_SQRT2; }}),
+        (UsefulFraction<double> {  "(%g) / (√2⋅%g)", [](double x) { return x * M_SQRT2; }}),
+    };
+
+    double value = 2.0 / 3.0 / std::sin(M_PI / 4);//2.0 / 3.0 * M_PI;//19.0 / 30.0 * M_PI;
+
+    for(auto i: uses)
     {
-        std::cout << "Not found\n";
+        auto x = i.operate(value);
+        auto f = Fraction<double>::find(x, 1E-30, 1000);
+        if (!f.isnan() && f.numerator < 1000)
+        {
+            printf(i.format.c_str(), f.numerator, f.denominator);
+            printf("\n");
+            break;
+        }
     }
+
+    return 0;
 }
