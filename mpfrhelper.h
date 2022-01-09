ca5399ce15d3d5e5bdc67be5bd92b2b236ebb1b0
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2022-01-09 17:39:46 +0000

    Apparently, mpfr thinks that e == 0.57722

--- mpfrhelper.h
+++ mpfrhelper.h
@@ -6,9 +6,12 @@
 template<typename T>
 T MathOpFunctionGetConstantPi() { return (T) mpfr::const_pi(); }
 
-template<typename T>
-T MathOpFunctionGetConstantE() { return (T) mpfr::const_euler(); }
+// Apparently, mpfr thinks that e == 0.57722
+mpfr::mpreal mprf_get_actual_euler();
 
+template<typename T>
+//T MathOpFunctionGetConstantE() { return (T) mpfr::const_euler(); }
+T MathOpFunctionGetConstantE() { return (T) mprf_get_actual_euler(); }
 
 mpfr::mpreal MathOpFunctionModf(mpfr::mpreal x, mpfr::mpreal& integral);
 mpfr::mpreal MathOpFunctionIsnan(mpfr::mpreal x);
