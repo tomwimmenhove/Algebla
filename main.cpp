e7799c1ed7ca34e947da43a1096ca648f2d3317d
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2022-01-09 00:22:30 +0000

    Persistent history

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "config.h"
 
 #include <iostream>
+#include <fstream>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 int main(int, char **)
 {
@@ -16,15 +20,36 @@ int main(int, char **)
 
     driver drv;
 
+    /* Read history */
+    std::string last_line;
+    const std::string history_path = std::string(getpwuid(getuid())->pw_dir) + "/.algebla_history";//"~/.algebla_history";
+    std::ifstream history_file(history_path);
+    std::string history_line;
+    while (std::getline(history_file, history_line))
+    {
+        last_line = history_line;
+        add_history(history_line.c_str());
+    }
+
     char *line_buf;
+    rl_bind_key('\t', rl_insert);
     while ((line_buf = readline("> ")) != nullptr)
     {
-        if (strlen(line_buf) > 0)
+        std::string line(line_buf);
+
+        if (!line.empty())
         {
-            add_history(line_buf);
+            if (line != last_line)
+            {
+                add_history(line_buf);
+                std::ofstream history_file(history_path, std::ios_base::app | std::ios_base::out);
+                history_file << line << '\n';
+            }
+
+            last_line = line;
         }
 
-        if (drv.parse_string(line_buf) != 0)
+        if (drv.parse_string(line) != 0)
         {
             continue;
         }
