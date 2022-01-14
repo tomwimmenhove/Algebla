037c8993f6c6b5d522b3800415598e41ec6cd454
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2022-01-14 21:56:53 +0000

    Helpers to header-only

--- mathop/mpfrhelper.h
+++ mathop/mpfrhelper.h
@@ -12,19 +12,19 @@ T get_constant_pi() { return boost::math::constants::pi<T>(); }
 template<typename T>
 T get_constant_e() { return boost::math::constants::e<T>(); }
 
-boost::multiprecision::mpfr_float modf(boost::multiprecision::mpfr_float x, boost::multiprecision::mpfr_float& integral);
-boost::multiprecision::mpfr_float isnan(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float abs(boost::multiprecision::mpfr_float x);
+inline boost::multiprecision::mpfr_float modf(boost::multiprecision::mpfr_float x, boost::multiprecision::mpfr_float &integral) { return boost::multiprecision::modf(x, &integral); }
+inline boost::multiprecision::mpfr_float isnan(boost::multiprecision::mpfr_float x) { return boost::multiprecision::isnan(x); }
+inline boost::multiprecision::mpfr_float abs(boost::multiprecision::mpfr_float x) { return boost::multiprecision::abs(x); }
 
-boost::multiprecision::mpfr_float log(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float sqrt(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float sin(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float asin(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float cos(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float acos(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float tan(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float atan(boost::multiprecision::mpfr_float x);
-boost::multiprecision::mpfr_float pow(boost::multiprecision::mpfr_float a, boost::multiprecision::mpfr_float b);
+inline boost::multiprecision::mpfr_float log(boost::multiprecision::mpfr_float x) { return boost::multiprecision::log(x); }
+inline boost::multiprecision::mpfr_float sqrt(boost::multiprecision::mpfr_float x) { return boost::multiprecision::sqrt(x); }
+inline boost::multiprecision::mpfr_float sin(boost::multiprecision::mpfr_float x) { return boost::multiprecision::sin(x); }
+inline boost::multiprecision::mpfr_float asin(boost::multiprecision::mpfr_float x) { return boost::multiprecision::asin(x); }
+inline boost::multiprecision::mpfr_float cos(boost::multiprecision::mpfr_float x) { return boost::multiprecision::cos(x); }
+inline boost::multiprecision::mpfr_float acos(boost::multiprecision::mpfr_float x) { return boost::multiprecision::acos(x); }
+inline boost::multiprecision::mpfr_float tan(boost::multiprecision::mpfr_float x) { return boost::multiprecision::tan(x); }
+inline boost::multiprecision::mpfr_float atan(boost::multiprecision::mpfr_float x) { return boost::multiprecision::atan(x); }
+inline boost::multiprecision::mpfr_float pow(boost::multiprecision::mpfr_float a, boost::multiprecision::mpfr_float b) { return boost::multiprecision::pow(a, b); }
 
 } /* namespace MathOps */
 
--- mathop/stdhelper.h
+++ mathop/stdhelper.h
@@ -15,49 +15,49 @@ template<typename T>
 //T get_constant_e() { return std::numbers::e_v<T>; }
 T get_constant_e() { return (T) (M_El); }
 
-float modf(float x, float& integral);
-float isnan(float x);
-float abs(float x);
+inline float modf(float x, float& integral) { return std::modf(x, &integral); }
+inline float isnan(float x) { return std::isnan(x); }
+inline float abs(float x) { return std::abs(x); }
 
-float log(float x);
-float sqrt(float x);
-float sin(float x);
-float asin(float x);
-float cos(float x);
-float acos(float x);
-float tan(float x);
-float atan(float x);
-float pow(float a, float b);
-
-
-double modf(double x, double& integral);
-double isnan(double x);
-double abs(double x);
-
-double log(double x);
-double sqrt(double x);
-double sin(double x);
-double asin(double x);
-double cos(double x);
-double acos(double x);
-double tan(double x);
-double atan(double x);
-double pow(double a, double b);
+inline float log(float x) { return std::log(x); }
+inline float sqrt(float x) { return std::sqrt(x); }
+inline float sin(float x) { return std::sin(x); }
+inline float asin(float x) { return std::asin(x); }
+inline float cos(float x) { return std::cos(x); }
+inline float acos(float x) { return std::acos(x); }
+inline float tan(float x) { return std::tan(x); }
+inline float atan(float x) { return std::atan(x); }
+inline float pow(float a, float b) { return std::pow(a, b); }
 
 
-long double modf(long double x, long double& integral);
-long double isnan(long double x);
-long double abs(long double x);
+inline double modf(double x, double& integral) { return std::modf(x, &integral); }
+inline double isnan(double x) { return std::isnan(x); }
+inline double abs(double x) { return std::abs(x); }
 
-long double log(long double x);
-long double sqrt(long double x);
-long double sin(long double x);
-long double asin(long double x);
-long double cos(long double x);
-long double acos(long double x);
-long double tan(long double x);
-long double atan(long double x);
-long double pow(long double a, long double b);
+inline double log(double x) { return std::log(x); }
+inline double sqrt(double x) { return std::sqrt(x); }
+inline double sin(double x) { return std::sin(x); }
+inline double asin(double x) { return std::asin(x); }
+inline double cos(double x) { return std::cos(x); }
+inline double acos(double x) { return std::acos(x); }
+inline double tan(double x) { return std::tan(x); }
+inline double atan(double x) { return std::atan(x); }
+inline double pow(double a, double b) { return std::pow(a, b); }
+
+
+inline long double modf(long double x, long double& integral) { return std::modf(x, &integral); }
+inline long double isnan(long double x) { return std::isnan(x); }
+inline long double abs(long double x) { return std::abs(x); }
+
+inline long double log(long double x) { return std::log(x); }
+inline long double sqrt(long double x) { return std::sqrt(x); }
+inline long double sin(long double x) { return std::sin(x); }
+inline long double asin(long double x) { return std::asin(x); }
+inline long double cos(long double x) { return std::cos(x); }
+inline long double acos(long double x) { return std::acos(x); }
+inline long double tan(long double x) { return std::tan(x); }
+inline long double atan(long double x) { return std::atan(x); }
+inline long double pow(long double a, long double b) { return std::pow(a, b); }
 
 } /* namespace MathOps */
 
